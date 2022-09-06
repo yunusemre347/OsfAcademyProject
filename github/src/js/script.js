2825143fb1fb8df9a87c7e3061388b28210317aa
yunusemre347 <yunusemreyilmaz347@gmail.com>
2022-09-06 13:24:12 +0300

    osfAcademyProject
    eylül 6
    ana sayfa hemen hemen bitti.
    load more butonu jsonla calıstırılacak.
    fav ve cart itemlerine counter eklenecek.

--- github/src/js/script.js
+++ github/src/js/script.js
@@ -26,5 +26,52 @@ productContainers.forEach((item,i) => {
 
 })
 
+const dateYear = new Date();
+document.getElementById("date-year").innerHTML=dateYear.getFullYear();
+
+// const btn = document.querySelector('#btn');
+//  const output= document.querySelector('.load-more-container');
+// const url='data.json';
+
+//  console.log(btn);
+//  btn.onclick =()=>{
+//     console.log('clicked');
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data =>{
+//        console.log(data);
+//     })
+//  }
+
+let btn = document.getElementById("load-more-btn");
+let loadMoreContainer = document.getElementById("load-more-container")
+let pageCounter =1;
+
+btn.addEventListener("click",function() {
+    let ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-2.json');
+    ourRequest.onload = function() {
+        let ourData = JSON.parse(ourRequest.responseText)
+        renderHTML(ourData);
+    };
+    ourRequest.send();
+    pageCounter++;
+
+});
+
+function renderHTML(data){
+    let htmlString = "";
+
+    for (i = 0 ; i <data.length; i++) {
+        htmlString += "<p>" + data [i].name + " is a" +data[i].species+ ".</p>";
+    }
+
+    loadMoreContainer.insertAdjacentHTML('beforeend',htmlString)
 
 
+    if (pageCounter > 1) {
+        btn.classList.add("hide-me")
+    }
+    
+
+}
